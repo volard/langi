ec0a55df209be441c31f1ec8d0f115a2788db1f5
volard <kirillzhilenkov@gmail.com>
2023-08-17 15:13:30 +0800

    :card_file_box: Add reactive MongoDB

--- src/main/java/com/volard/langi/WebConfiguration.java
+++ src/main/java/com/volard/langi/WebConfiguration.java
@@ -1,7 +1,28 @@
 package com.volard.langi;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class WebConfiguration {
+    final String FRONTEND_URL = "*";
+
+    @Bean
+    CorsWebFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(FRONTEND_URL);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
 }
