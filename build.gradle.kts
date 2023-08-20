1635fd0987342825d95437c0ecfb711ef7814222
volard <kirillzhilenkov@gmail.com>
2023-08-20 14:01:57 +0800

    :construction: Add JWT service

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.projectlombok:lombok:1.18.26")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // jjwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
---
+++ src/main/java/com/volard/langi/security/JwtService.java
@@ -0,0 +1,34 @@
+package com.volard.langi.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.security.Keys;
+
+import java.security.KeyPair;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+    private final int timeOffset = 60 * 15;
+    private final KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+
+    public String createJwt(String userId){
+        return Jwts.builder()
+                .setIssuer("volard")
+                .setSubject(userId)
+                .setAudience("you")
+                .setExpiration(new Date(System.currentTimeMillis() + timeOffset))
+                .setIssuedAt((new Date()))
+                .setNotBefore(new Date())
+                .setId(UUID.randomUUID().toString())
+                .signWith(keyPair.getPrivate(), SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    public Jwt readJwt(String jwt){
+        JwtParser parser = Jwts.parserBuilder().setSigningKey(keyPair.getPublic()).build();
+        return parser.parseClaimsJwt(jwt);
+    }
+}
