6376745304d326307b36d2a054044f1b6cb0354b
volard <kirillzhilenkov@gmail.com>
2023-09-01 08:17:54 +0800

    :closed_lock_with_key: Move out secrets

--- src/main/java/com/volard/langi/security/utils/JwtService.java
+++ src/main/java/com/volard/langi/security/utils/JwtService.java
@@ -1,32 +1,51 @@
-package com.volard.langi.security;
+package com.volard.langi.security.utils;
 
-import io.jsonwebtoken.*;
-import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import java.security.KeyPair;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 import java.util.UUID;
 
+/**
+ * Service to handle (generate and verify) JWT
+ */
 @Service
 public class JwtService {
-    // todo hide fields in secrets or somewhere and change them
-    private final int expirationTimeMinutes = 15;
-    private final String audience = "langi_users";
-    private final String issuer = "volard";
-    private final KeyPair keyPair = Keys.keyPairFor(SignatureAlgorithm.RS256);
+    @Value("${jwt.expiration}")
+    private int expirationTimeInSeconds;
+
+    @Value("${jwt.audience}")
+    private String audience;
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.RS256;
+
+    private final KeyPair keyPair = Keys.keyPairFor(signatureAlgorithm);
 
     public String createJwt(String userId){
+
+
         return Jwts.builder()
+
                 .setIssuer(issuer)
                 .setSubject(userId)
                 .setAudience(audience)
-                .setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(expirationTimeMinutes))))
+                .setExpiration(Date.from(Instant.now().plus(Duration.ofSeconds(expirationTimeInSeconds))))
                 .setIssuedAt(Date.from(Instant.now()))
                 .setNotBefore(Date.from(Instant.now()))
                 .setId(UUID.randomUUID().toString())
+
                 .signWith(keyPair.getPrivate(), SignatureAlgorithm.RS256)
+
                 .compact();
     }
 
